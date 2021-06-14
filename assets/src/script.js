ea5953d497e76501c7a66bf2fe42d89bb862f2a5
Saddam Satria <ardhisaddam@gmail.com>
2021-06-14 17:22:48 +0700

    Update  Social Media

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,8 +1,22 @@
-const h5 = document.querySelector('.whatsapp h5')
-const mouseOver = () =>{
-    h5.style.display = 'block';
-}
+const h5 = document.querySelector('.whatsapp h5');
+const mouseOver = () => {
+  h5.style.display = 'block';
+};
 
-const mouseOut = () =>{
-    h5.style.display = 'none';
-}   
+const mouseOut = () => {
+  h5.style.display = 'none';
+};
+
+const socialMenu = document.querySelector('.social');
+const whatsapp = document.querySelector('.whatsapp');
+
+socialMenu.onclick = function () {
+  window.scrollTo(pageXOffset, 0);
+  // after scrollTo, there will be a "scroll" event, so the arrow will hide automatically
+};
+
+window.addEventListener('scroll', function () {
+  socialMenu.hidden = pageYOffset < document.documentElement.clientHeight;
+  whatsapp.hidden = pageYOffset < document.documentElement.clientHeight;
+  
+});
